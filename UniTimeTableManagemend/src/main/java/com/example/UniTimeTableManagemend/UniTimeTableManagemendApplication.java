7a5b2e2b90ad72c80eeba25a65d2151b76553eae
Sudhais <87361938+sudhais@users.noreply.github.com>
2024-03-20 21:13:09 +0530

    initial project

---
+++ UniTimeTableManagemend/src/main/java/com/example/UniTimeTableManagemend/UniTimeTableManagemendApplication.java
@@ -0,0 +1,13 @@
+package com.example.UniTimeTableManagemend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniTimeTableManagemendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniTimeTableManagemendApplication.class, args);
+	}
+
+}
